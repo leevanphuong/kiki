11bffc1731f4eb175c2ad3cdc254399e1ff0ffe6
phuongpew@gmail.com <phuongpew@gmail.com>
2023-07-31 20:47:49 +0700

    style: fe-create-componet-ui-product-detail-of-the-client Hoàn thành phần create ui detail product ref: DTNF-16

--- FE/src/app/modules/client/router.tsx
+++ FE/src/app/modules/client/router.tsx
@@ -1,8 +1,13 @@
 import { RouteObject } from "react-router-dom"
 import Home from "./home/home.component"
+import Detail from "./detail/detail.component"
 export const clientRouter: RouteObject[] = [
     {
         path: "/",
         element: <Home />
+    },
+    {
+        path: "/detail",
+        element: <Detail/>
     }
 ]
